b86e590674601c2191dd94775ea97ad4ddf6feee
Andries Seutens <andries@centim.be>
2012-10-28 12:10:47 +0100

    Fixed bug in writing single coil. 0xff00 = on, 0x0000 = off.

--- library/Stca/Modbus/Message/WriteSingleCoil.php
+++ library/Stca/Modbus/Message/WriteSingleCoil.php
@@ -4,8 +4,20 @@ namespace Stca\Modbus\Message;
 
 use InvalidArgumentException;
 
+/**
+ * This function code is used to write a single output to either ON or OFF in a remote device. The requested ON/OFF
+ * state is specified by a constant in the request data field. A value of FF 00 hex requests the output to be ON.
+ * A value of 00 00 requests it to be OFF. All other values are illegal and will not affect the output. The Request
+ * PDU specifies the address of the coil to be forced. Coils are addressed starting at zero. Therefore coil numbered
+ * 1 is addressed as 0. The requested ON/OFF state is specified by a constant in the Coil Value field. A value of
+ * 0XFF00 requests the coil to be ON. A value of 0X0000 requests the coil to be off. All other values are illegal and
+ * will not affect the coil.
+ */
 class WriteSingleCoil extends ReadSingleCoil
 {
+    const ON  = 0xff00;
+    const OFF = 0x0000;
+
     /**
      * @var boolean
      */
@@ -34,8 +46,8 @@ class WriteSingleCoil extends ReadSingleCoil
      */
     public function setValue($value)
     {
-        if (!is_bool($value)) {
-            throw new InvalidArgumentException('Value should be a boolean, got: ' . gettype($value));
+        if ($value !== self::ON && $value !== self::OFF) {
+            throw new InvalidArgumentException('Invalid coil value. See WriteSingleCoil::OFF and WriteSingleCoil::ON');
         }
 
         $this->value = $value;
@@ -43,7 +55,7 @@ class WriteSingleCoil extends ReadSingleCoil
     }
 
     /**
-     * Returns coil value (true = on, false = off)
+     * Returns coil value
      *
      * @return bool
      */
